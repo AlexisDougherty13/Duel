c317cc46d378243ab928b170dbc13cdeb965f425
AlexisDougherty13 <alexisdougherty@ufl.edu>
2020-06-02 15:07:17 -0400

    Latest stuff during 2 June meeting

--- player.py
+++ player.py
@@ -1,55 +1,123 @@
 import time
 
+#change these
+shift_size = 50
+hit_box_width = 102
+hit_box_height = 140
+
+# 102x203
+
 class Player:
-    def __init__(self, xpos, ypos, directionFacing, sprite):
-        self._xpos = xpos
-        self._ypos = ypos
-        self._directionFacing = directionFacing
+    def __init__(self, x_pos, y_pos, direction_facing, sword_height, is_ghost, sprite):
+        self._x_pos = x_pos
+        self._y_pos = y_pos
+        self._direction_facing = direction_facing
+        self._sword_height = sword_height
+        self._is_ghost = is_ghost
         self._sprite = sprite
         
-    def get_xpos(self):
-        return self._xpos
+    def getXPos(self):
+        return self._x_pos
         
-    def set_xpos(self, newXPos):
-        self._xpos = newXPos
+    def setXPos(self, new_x_pos):
+        self._x_pos = new_x_pos
         
-    def get_ypos(self):
-        return self._ypos
+    def getYPos(self):
+        return self._y_pos
         
-    def set_ypos(self, newYPos):
-        self._ypos = newYPos
+    def setYPos(self, new_y_pos):
+        self._y_pos = new_y_pos
         
-    def get_directionFacing(self):
-        return self.__directionFacing
+    def getDirectionFacing(self):
+        return self._direction_facing
         
     def switchDirection(self):
-        if(self._xpos):
-            self._xpos = False
+        if(self._x_pos):
+            self._x_pos = False
         else:
-            self._xpos = True
+            self._x_pos = True
         
-    def get_sprite(self):
+    # 0 means no sword, 3 is high, 2 is med, 1 is low sword
+    def getSwordHeight(self):
+        return self._sword_height
+        
+    def setSwordHeight(self, new_pos):
+        self._sword_height = new_pos
+        
+    def getIsGhost(self):
+        return self._is_ghost
+        
+    def setIsGhost(self):
+        if(self._is_ghost):
+            self._is_ghost = False
+        else:
+            self._is_ghost = True
+        
+    def getSprite(self):
         return self._sprite
         
-    def set_sprite(self, sprite):
+    def setSprite(self, sprite):
         self._sprite = sprite  
+        print("New: ", self._sprite)
+        
+    #def insert_sting_middle(self, str, word):
+        #return str[:2] + word + str[2:]
+        
+    #def reverseImage(self):
+        #if_r = self._sprite.find("R")
+        #if(if_r == -1):
+            #self._sprite = self.insert_string_middle(self._sprite, "R")
+            #print(self._sprite)
        
     def moveLeft(self):
-        if(self._directionFacing == 0):
-            self._xpos = self._xpos - 50
+        global shift_size
+        if(self._direction_facing == 0):
+            self._x_pos = self._x_pos - shift_size
         else:
-            self._directionFacing = 0
+            self._direction_facing = 0
+            #self.reverseImage()
+            self._sprite = "FillerSpriteR.png"
         
     def moveRight(self):
-        if(self._directionFacing == 1):
-            self._xpos = self._xpos + 50
+        global shift_size
+        if(self._direction_facing == 1):
+            self._x_pos = self._x_pos + shift_size
         else:
-            self._directionFacing = 1
+            self._direction_facing = 1
+            #reverseImage()
+            self._sprite = "FillerSprite.png"
+           
+    # 0 means no sword, 3 is high, 2 is med, 1 is low sword
+    def raiseSword(self):
+        if(self._sword_height >= 1 and self._sword_height < 3):
+            self._sword_height += 1
+            
+    def lowerSword(self):
+        if(self._sword_height <= 3 and self._sword_height > 1):
+            self._sword_height -= 1
+            
+    def duck(self):
+        global hit_box_height
+        hit_box_height /= 2
         
-    xpos = property(get_xpos, set_xpos)
-    ypos = property(get_ypos, set_ypos)
-    directionFacing = property(get_directionFacing, switchDirection)
-    sprite = property(get_sprite, set_sprite)
+    def standUp(self):
+        global hit_box_height
+        hit_box_height *= 2
+        
+    x_pos = property(getXPos, setXPos)
+    y_pos = property(getYPos, setYPos)
+    direction_facing = property(getDirectionFacing, switchDirection)
+    sword_height = property(getSwordHeight, setSwordHeight)
+    is_ghost = property(getIsGhost, setIsGhost)
+    sprite = property(getSprite, setSprite)
     
-    moveLeft = property(moveLeft)
-    moveRight = property(moveRight)
+    move_left = property(moveLeft)
+    move_right = property(moveRight)
+    raise_sword = property(raiseSword)
+    lower_sword = property(lowerSword)
+    duck = property(duck)
+    standUp = property(standUp)
+
+
+# Sources:
+# https://www.w3resource.com/python-exercises/string/python-data-type-string-exercise-16.php
