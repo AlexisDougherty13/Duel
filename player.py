54e784d9ea5a012e20b95adcea00b8e79dacf6fb
Legacy35 <44005399+Legacy35@users.noreply.github.com>
2020-06-13 13:51:16 -0400

    File struture changes

--- player.py
+++ player.py
@@ -13,7 +13,7 @@ hit_box_height = 140
 
 class Player:
     """Class representing individual players' avatar, their attributes, and movement
-       :param x_pos: the player's x-coordinate 
+       :param x_pos: the player's x-coordinate
        :param y_pos: the player's y-coordinate
        :param direction_facing: the direction the player is facing (0 for left and 1 for right)
        :param sword_height: the height at which the player is holding the sword (0 means no sword, 3 is high, 2 is med, 1 is low sword)
@@ -30,80 +30,80 @@ class Player:
         self._is_ghost = is_ghost
         self._is_locked_on = is_locked_on
         self._sprite = sprite
-        
+
     def getXPos(self):
         return self._x_pos
-        
+
     def setXPos(self, new_x_pos):
         self._x_pos = new_x_pos
-        
+
     def getYPos(self):
         return self._y_pos
-        
+
     def setYPos(self, new_y_pos):
         self._y_pos = new_y_pos
-        
+
     def getDirectionFacing(self):
         return self._direction_facing
-        
+
     def switchDirection(self):
         if(self._x_pos):
             self._x_pos = False
         else:
             self._x_pos = True
-        
+
     # 0 means no sword, 3 is high, 2 is med, 1 is low sword
     def getSwordHeight(self):
         return self._sword_height
-        
+
     def setSwordHeight(self, new_pos):
         self._sword_height = new_pos
-        
+
     def getIsGhost(self):
         return self._is_ghost
-        
+
     def setIsGhost(self):
         if(self._is_ghost):
             self._is_ghost = False
         else:
             self._is_ghost = True
-            
+
     def getIsLockedOn(self):
         return self._is_locked_on
-        
+
     def setIsLockedOn(self):
         if(self._is_locked_on):
             self._is_locked_on = False
         else:
             self._is_locked_on = True
-        
+
     def getSprite(self):
         return self._sprite
-        
+
     def setSprite(self, sprite):
-        self._sprite = sprite  
-       
+        self._sprite = sprite
+
     def moveLeft(self, time):
         self._x_pos -= 3.0
         if time>=0.25:
             self._direction_facing = 0
-            self._sprite = "FillerSpriteR.png"   
-        
+            self._sprite = "FillerSpriteR.png"
+
     def moveRight(self, time):
         self._x_pos += 3.0
         if time>=0.25:
             self._direction_facing = 1
             self._sprite = "FillerSpriteL.png"
-           
+
     # 0 means no sword, 3 is high, 2 is med, 1 is low sword
     def raiseSword(self):
         if(self._sword_height >= 1 and self._sword_height < 3):
             self._sword_height += 1
-            
+
     def lowerSword(self):
         if(self._sword_height <= 3 and self._sword_height > 1):
             self._sword_height -= 1
-            
+
     def duck(self):
         global hit_box_height
         hit_box_height /= 2
@@ -111,7 +111,7 @@ class Player:
     def standUp(self):
         global hit_box_height
         hit_box_height *= 2
-        
+
     x_pos = property(getXPos, setXPos)
     y_pos = property(getYPos, setYPos)
     direction_facing = property(getDirectionFacing, switchDirection)
@@ -119,7 +119,7 @@ class Player:
     is_ghost = property(getIsGhost, setIsGhost)
     is_locked_on = property(getIsLockedOn, setIsLockedOn)
     sprite = property(getSprite, setSprite)
-    
+
     move_left = property(moveLeft)
     move_right = property(moveRight)
     raise_sword = property(raiseSword)
