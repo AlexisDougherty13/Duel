7ecc7820c934060f34c1173ebf8c3bd90706f903
gracewilcox <graceawilcox@gmail.com>
2020-06-19 20:17:15 -0400

    integrated some changes

--- swordHitBoxes.py
+++ swordHitBoxes.py
@@ -8,23 +8,23 @@ def getSwordLine(player):
 	if abs(player.getXVelocity()) < 30: #if sword is ready
 		if player.getDirectionFacing() == "left":
 			if player.getIsAttacking() == False: #if player is not attacking
-				point1 = player.player_rect.x + 14
-				point2 = player.player_rect.x + 69
+				point1 = player.rect.x + 14
+				point2 = player.rect.x + 69
 			else: #player is attacking
-				point1 = player.player_rect.x + 0
-				point2 = player.player_rect.x + 55
+				point1 = player.rect.x + 0
+				point2 = player.rect.x + 55
 		else:
 			if player.getIsAttacking() == False: #if player is not attacking
-				point1 = player.player_rect.x + 145
-				point2 = player.player_rect.x + 200
+				point1 = player.rect.x + 145
+				point2 = player.rect.x + 200
 			else: #player is attacking
-				point1 = player.player_rect.x + 159
-				point2 = player.player_rect.x + 214
+				point1 = player.rect.x + 159
+				point2 = player.rect.x + 214
 		if player.getSwordHeight() == 1:
-			height = player.player_rect.y + 61
+			height = player.rect.y + 61
 		elif player.getSwordHeight() == 2:
-			height = player.player_rect.y + 37
+			height = player.rect.y + 37
 		elif player.getSwordHeight() == 3:
-			height = player.player_rect.y + 13
+			height = player.rect.y + 13
 	return Rect(point1, height, point2-point1, 3)
 
