36a4ccba0185f907ef714b7b145b29df2318547a
AlexisDougherty13 <alexisdougherty@ufl.edu>
2020-07-20 17:01:39 -0400

    added individual swords

--- playerSkinsList.py
+++ playerSkinsList.py
@@ -19,10 +19,125 @@
 # "thrust_low_r": ""}
 
 
+#NOTE
+#UNTIL AN IMAGE IS CREATED FOR THE OTHER CHARACTERS, THE DEFAULT IS MONTOYA
+#I WILL CHANGE THE PATHS WHEN I ADD IMAGES 
+# - ALEXIS
+
+
 def getSkin(character_type):
     images_dictionary = dict()
     if character_type == "Montoya":
         images_dictionary = {
+            "sword": "Resources/Images/MontoyaSword.png",
+            "run_l": "Resources/Images/MontoyaRun1L.png",
+            "run_r": "Resources/Images/MontoyaRun1R.png",
+            "run_l_1": "Resources/Images/MontoyaRun1L.png",
+            "run_r_1": "Resources/Images/MontoyaRun1R.png",
+            "run_l_2": "Resources/Images/MontoyaRun2L.png",
+            "run_r_2": "Resources/Images/MontoyaRun2R.png",
+            "run_l_3": "Resources/Images/MontoyaRun3L.png",
+            "run_r_3": "Resources/Images/MontoyaRun3R.png",
+            "sword_high_l": "Resources/Images/MontoyaHighL.png",
+            "sword_high_r": "Resources/Images/MontoyaHighR.png",
+            "sword_med_l": "Resources/Images/MontoyaMedL.png",
+            "sword_med_r": "Resources/Images/MontoyaMedR.png",
+            "sword_low_l": "Resources/Images/MontoyaLowL.png",
+            "sword_low_r": "Resources/Images/MontoyaLowR.png",
+            "duck_l": "Resources/Images/MontoyaDuckL.png",
+            "duck_r": "Resources/Images/MontoyaDuckR.png",
+            "thrust_high_l": "Resources/Images/MontoyaHighThrustL.png",
+            "thrust_high_r": "Resources/Images/MontoyaHighThrustR.png",
+            "thrust_med_l": "Resources/Images/MontoyaMedThrustL.png",
+            "thrust_med_r": "Resources/Images/MontoyaMedThrustR.png",
+            "thrust_low_l": "Resources/Images/MontoyaLowThrustL.png",
+            "thrust_low_r": "Resources/Images/MontoyaLowThrustR.png",
+            "dead_l_1": "Resources/Images/MontoyaDeath1L.png",
+            "dead_r_1": "Resources/Images/MontoyaDeath1R.png",
+            "dead_l_2": "Resources/Images/MontoyaDeath2L.png",
+            "dead_r_2": "Resources/Images/MontoyaDeath2R.png",
+            "dead_l_3": "Resources/Images/MontoyaDeath3L.png",
+            "dead_r_3": "Resources/Images/MontoyaDeath3R.png",
+            "ghost_run_l": "Resources/Images/MontoyaGhostRunL.png",
+            "ghost_run_r": "Resources/Images/MontoyaGhostRunR.png",
+            "ghost_sword_high_l": "Resources/Images/MontoyaGhostHighL.png",
+            "ghost_sword_high_r": "Resources/Images/MontoyaGhostHighR.png",
+            "ghost_sword_med_l": "Resources/Images/MontoyaGhostMedL.png",
+            "ghost_sword_med_r": "Resources/Images/MontoyaGhostMedR.png",
+            "ghost_sword_low_l": "Resources/Images/MontoyaGhostLowL.png",
+            "ghost_sword_low_r": "Resources/Images/MontoyaGhostLowR.png",
+            "ghost_duck_l": "Resources/Images/MontoyaGhostDuckL.png",
+            "ghost_duck_r": "Resources/Images/MontoyaGhostDuckR.png",
+            "ghost_thrust_high_l": "Resources/Images/MontoyaGhostHighThrustL.png",
+            "ghost_thrust_high_r": "Resources/Images/MontoyaGhostHighThrustR.png",
+            "ghost_thrust_med_l": "Resources/Images/MontoyaGhostMedThrustL.png",
+            "ghost_thrust_med_r": "Resources/Images/MontoyaGhostMedThrustR.png",
+            "ghost_thrust_low_l": "Resources/Images/MontoyaGhostLowThrustL.png",
+            "ghost_thrust_low_r": "Resources/Images/MontoyaGhostLowThrustR.png",
+            "ghost_dead_l_1": "Resources/Images/MontoyaGhostDeath1L.png",
+            "ghost_dead_r_1": "Resources/Images/MontoyaGhostDeath1R.png",
+            "ghost_dead_l_2": "Resources/Images/MontoyaGhostDeath2L.png",
+            "ghost_dead_r_2": "Resources/Images/MontoyaGhostDeath2R.png",
+            "ghost_dead_l_3": "Resources/Images/MontoyaGhostDeath3L.png",
+            "ghost_dead_r_3": "Resources/Images/MontoyaGhostDeath3R.png"
+        }
+    elif character_type == "Zorro":
+        images_dictionary = {
+            "sword": "Resources/Images/ZorroSword.png",
+            "run_l": "Resources/Images/MontoyaRun1L.png",
+            "run_r": "Resources/Images/MontoyaRun1R.png",
+            "run_l_1": "Resources/Images/MontoyaRun1L.png",
+            "run_r_1": "Resources/Images/MontoyaRun1R.png",
+            "run_l_2": "Resources/Images/MontoyaRun2L.png",
+            "run_r_2": "Resources/Images/MontoyaRun2R.png",
+            "run_l_3": "Resources/Images/MontoyaRun3L.png",
+            "run_r_3": "Resources/Images/MontoyaRun3R.png",
+            "sword_high_l": "Resources/Images/MontoyaHighL.png",
+            "sword_high_r": "Resources/Images/MontoyaHighR.png",
+            "sword_med_l": "Resources/Images/MontoyaMedL.png",
+            "sword_med_r": "Resources/Images/MontoyaMedR.png",
+            "sword_low_l": "Resources/Images/MontoyaLowL.png",
+            "sword_low_r": "Resources/Images/MontoyaLowR.png",
+            "duck_l": "Resources/Images/MontoyaDuckL.png",
+            "duck_r": "Resources/Images/MontoyaDuckR.png",
+            "thrust_high_l": "Resources/Images/MontoyaHighThrustL.png",
+            "thrust_high_r": "Resources/Images/MontoyaHighThrustR.png",
+            "thrust_med_l": "Resources/Images/MontoyaMedThrustL.png",
+            "thrust_med_r": "Resources/Images/MontoyaMedThrustR.png",
+            "thrust_low_l": "Resources/Images/MontoyaLowThrustL.png",
+            "thrust_low_r": "Resources/Images/MontoyaLowThrustR.png",
+            "dead_l_1": "Resources/Images/MontoyaDeath1L.png",
+            "dead_r_1": "Resources/Images/MontoyaDeath1R.png",
+            "dead_l_2": "Resources/Images/MontoyaDeath2L.png",
+            "dead_r_2": "Resources/Images/MontoyaDeath2R.png",
+            "dead_l_3": "Resources/Images/MontoyaDeath3L.png",
+            "dead_r_3": "Resources/Images/MontoyaDeath3R.png",
+            "ghost_run_l": "Resources/Images/MontoyaGhostRunL.png",
+            "ghost_run_r": "Resources/Images/MontoyaGhostRunR.png",
+            "ghost_sword_high_l": "Resources/Images/MontoyaGhostHighL.png",
+            "ghost_sword_high_r": "Resources/Images/MontoyaGhostHighR.png",
+            "ghost_sword_med_l": "Resources/Images/MontoyaGhostMedL.png",
+            "ghost_sword_med_r": "Resources/Images/MontoyaGhostMedR.png",
+            "ghost_sword_low_l": "Resources/Images/MontoyaGhostLowL.png",
+            "ghost_sword_low_r": "Resources/Images/MontoyaGhostLowR.png",
+            "ghost_duck_l": "Resources/Images/MontoyaGhostDuckL.png",
+            "ghost_duck_r": "Resources/Images/MontoyaGhostDuckR.png",
+            "ghost_thrust_high_l": "Resources/Images/MontoyaGhostHighThrustL.png",
+            "ghost_thrust_high_r": "Resources/Images/MontoyaGhostHighThrustR.png",
+            "ghost_thrust_med_l": "Resources/Images/MontoyaGhostMedThrustL.png",
+            "ghost_thrust_med_r": "Resources/Images/MontoyaGhostMedThrustR.png",
+            "ghost_thrust_low_l": "Resources/Images/MontoyaGhostLowThrustL.png",
+            "ghost_thrust_low_r": "Resources/Images/MontoyaGhostLowThrustR.png",
+            "ghost_dead_l_1": "Resources/Images/MontoyaGhostDeath1L.png",
+            "ghost_dead_r_1": "Resources/Images/MontoyaGhostDeath1R.png",
+            "ghost_dead_l_2": "Resources/Images/MontoyaGhostDeath2L.png",
+            "ghost_dead_r_2": "Resources/Images/MontoyaGhostDeath2R.png",
+            "ghost_dead_l_3": "Resources/Images/MontoyaGhostDeath3L.png",
+            "ghost_dead_r_3": "Resources/Images/MontoyaGhostDeath3R.png"
+        }
+    elif character_type == "King Arthur":
+        images_dictionary = {
+            "sword": "Resources/Images/MontoyaSword.png",
             "run_l": "Resources/Images/MontoyaRun1L.png",
             "run_r": "Resources/Images/MontoyaRun1R.png",
             "run_l_1": "Resources/Images/MontoyaRun1L.png",
@@ -77,6 +192,7 @@ def getSkin(character_type):
     else:
         # set a default skin
         images_dictionary = {
+            "sword": "Resources/Images/MontoyaSword.png",
             "run_l": "Resources/Images/MontoyaRun1L.png",
             "run_r": "Resources/Images/MontoyaRun1R.png",
             "run_l_1": "Resources/Images/MontoyaRun1L.png",
